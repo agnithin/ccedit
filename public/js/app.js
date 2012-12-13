d01fe9078d47e7c3d77a2b255aaa3140ebe56e86
Nithin Anand Gangadharan <agnithin@gmail.com>
2012-12-12 20:54:19 -0500

    added angular-ui

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,4 @@
 // Declare app level module which depends on filters, and services
 //var app = angular.module('myApp', ['myApp.filters', 'myApp.directives']);
 
-angular.module('ui.config', []).value('ui.config', {});
-angular.module('ui.directives', ['ui.config']);
-
-var app = angular.module('myApp', ['myApp.directives', 'ui.directives', 'ui.config']);
-
+var app = angular.module('myApp', ['myApp.directives', 'ui']);
